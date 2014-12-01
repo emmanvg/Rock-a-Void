99056257ae004a58d20195344b798fc510857e9a
cecilia <prettyhilgirl@yahoo.com>
2014-11-30 20:12:52 -0400

    working

--- Rock-a-Void/gen/edu/uprm/rock_a_void/R.java
+++ Rock-a-Void/gen/edu/uprm/rock_a_void/R.java
@@ -28,11 +28,12 @@ public final class R {
         public static final int sand_texture=0x7f020003;
     }
     public static final class id {
+        public static final int RankingsLayout=0x7f080007;
         public static final int RelativeLayout=0x7f080000;
         public static final int TextViewAbout=0x7f080006;
         public static final int TextViewRankings=0x7f080005;
         public static final int aboutImage=0x7f080004;
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f080008;
         public static final int rankingImage=0x7f080003;
         public static final int startGameImage=0x7f080001;
         public static final int textViewStartGame=0x7f080002;
